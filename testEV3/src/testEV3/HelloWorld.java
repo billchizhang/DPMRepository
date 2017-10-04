f7cc2a1e329cb300718b8b0e9bb0ea666b560be1
billchizhang <zhangchi6680@gmail.com>
2017-10-04 01:07:44 -0400

    initial testing class

---
+++ testEV3/src/testEV3/HelloWorld.java
@@ -0,0 +1,9 @@
+package testEV3;
+import lejos.hardware.Button; 
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld"); 
+		Button.waitForAnyPress(); 
+	}
+}
